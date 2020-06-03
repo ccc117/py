b706d734ef54f278ad437fcf7dce341ce452f5a6
ccc117 <ccc117@users.noreply.github.com>
2020-06-03 16:15:32 +0900

    simple

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import pygame
 import time
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 80
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 1200
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 time.sleep(3)
