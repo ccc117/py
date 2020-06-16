7772282e402c9ace5c397c724c19e5b7b3a3b2cf
ccc117 <ccc117@users.noreply.github.com>
2020-06-16 18:44:39 +0900

    test

--- game.py
+++
@@ -1,7 +0,0 @@
-import pygame
-import time
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 1200
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-time.sleep(3)
---
+++ test.py
@@ -0,0 +1,5 @@
+
+x=float(input('섭씨 온도를 입력하세요: '))
+print("섭씨 온도는", x, f"화씨 온도는 {x*9/5+32}")
+print({type(x)})
+print(index(0))
